0fac36b45c2fda4c62efbb5ae75c3deb4cfa0975
theantidoak <tcdoak@gmail.com>
2024-04-30 22:32:07 +0200

    style login form page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -24,6 +24,7 @@
 </script>
 
 <div class="form-container">
+  <h1 class="form-container-heading">Login Form</h1>
   <form id="login-form-id" action="" class="form" on:submit={submitForm}>
     <div class="form__field">
       <label for="email">Email: </label>
@@ -33,8 +34,65 @@
       <label for="password">Password: </label>
       <input type="password" name="password" required />
     </div>
-    <div class="form__field">
+    <div class="form__field buttons">
       <button class="form__submit-btn">Login</button>
     </div>
   </form>
 </div>
+
+<style lang="scss">
+  .form-container {
+    width: fit-content;
+    height: fit-content;
+    margin: 4rem auto;
+    padding: 1rem 1.25rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .3);
+  }
+
+  .form-container-heading {
+    color: var(--dark-blue);
+    font-weight: bold;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: .75rem;
+    width: fit-content;
+
+    &__field {
+      display: flex;
+      align-items: center;
+
+      &.buttons {
+        width: fit-content;
+        margin: 1rem auto .5rem;
+      }
+
+      label {
+        flex: 1;
+        padding: 0 .5rem;
+        font-size: clamp(14px, 4.5vw, 16px);
+      }
+
+      input {
+        padding: .25rem .5rem;
+      }
+    }
+
+    &__submit-btn {
+      padding: .5rem 1rem;
+      cursor: pointer;
+      font-size: 16px;
+      color: white;
+      background-color: var(--light-blue);
+      border-radius: 10px;
+      border: none;
+      box-shadow: 0 1px 2px rgba(0, 0, 0, .3);
+
+      &:hover, &:active {
+        box-shadow: 0 3px 6px rgba(0, 0, 0, .3);
+      }
+    }
+  }
+</style>
