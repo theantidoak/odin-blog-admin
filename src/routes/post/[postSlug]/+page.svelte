1705148b40c1ffc7c57be9072d6e313c822737ef
theantidoak <tcdoak@gmail.com>
2025-01-26 23:35:05 +0100

    test

--- src/routes/post/[postSlug]/+page.svelte
+++ src/routes/post/[postSlug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+  import { page } from '$app/stores'; 
   import { post, comments, isAdmin } from "../../../stores/writables";
   
   const { body } = $page.data;
